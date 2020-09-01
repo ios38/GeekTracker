02ad5414455b6e70e730a54867e56d5ed1da6891
Maksim Romanov <ios38ru@gmail.com>
2020-09-01 12:05:46 +0800

    Added GoogleMaps

---
+++ GeekTracker/App/MapController.swift
@@ -0,0 +1,30 @@
+//
+//  MapController.swift
+//  GeekTracker
+//
+//  Created by Maksim Romanov on 01.09.2020.
+//  Copyright © 2020 Maksim Romanov. All rights reserved.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
